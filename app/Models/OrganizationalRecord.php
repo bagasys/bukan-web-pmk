6e2fe54e1bed59352c57960fb7f84484bcc0ce23
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-05 21:57:18 +0700

    Update OrganizationalRecord.php
    menambahkan relasi

--- app/Models/OrganizationalRecord.php
+++ app/Models/OrganizationalRecord.php
@@ -19,4 +19,9 @@ class OrganizationalRecord extends Model
         'year_end',
         'category'
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
