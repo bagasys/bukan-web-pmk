75d8292ed204e0698e153414271ee834ead40033
Jeremy Vijay Wongso <37033334+jeremyvw@users.noreply.github.com>
2020-10-05 19:14:53 +0700

    Add prayer request migration

---
+++ app/Http/Controllers/PrayerRequestController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\PrayerRequest;
+use Illuminate\Http\Request;
+
+class PrayerRequestController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\PrayerRequest  $prayerRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function show(PrayerRequest $prayerRequest)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\PrayerRequest  $prayerRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(PrayerRequest $prayerRequest)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\PrayerRequest  $prayerRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, PrayerRequest $prayerRequest)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\PrayerRequest  $prayerRequest
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(PrayerRequest $prayerRequest)
+    {
+        //
+    }
+}
---
+++ app/Models/PrayerRequest.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class PrayerRequest extends Model
+{
+    use HasFactory;
+
+    protected $table = 'prayer_requests';
+
+    protected $fillable = [
+        'name',
+        'prayer_content',
+        'status',
+    ];
+}
