5b95fe6600aa2f97ced9f282924ed5340c2ae618
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-05 18:29:48 +0700

    Update index.blade.php
    tombol tambah dosen

--- resources/views/lecturers/index.blade.php
+++ resources/views/lecturers/index.blade.php
@@ -10,12 +10,11 @@
     <div class="card">
         <div class="card-header">
             <h3 class="card-title">Dosen PMK ITS</h3>
+
+
             <div class="card-tools">
-                <div class="input-group input-group-sm" style="width: 150px;">
-                    <input type="text" name="table_search" class="form-control float-right" placeholder="Search">
-                    <div class="input-group-append">
-                        <button type="submit" class="btn btn-default"><i class="fas fa-search"></i></button>
-                    </div>
+                <div class="">
+                    <a class="btn btn-success" href="{{ route('lecturers.create') }}"> Tambah Data Dosen</a>
                 </div>
             </div>
         </div>
@@ -53,4 +52,4 @@
         </div>
     </div>
 
-@endsection
+
