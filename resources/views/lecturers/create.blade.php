5e29853bd380999819c79e2a47aa87749ab5dbd0
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-05 17:10:12 +0700

    Update create.blade.php

--- resources/views/lecturers/create.blade.php
+++ resources/views/lecturers/create.blade.php
@@ -1,81 +1,109 @@
-@extends('lecturers.layout')
-  
+@extends('adminlte.master')
+
 @section('content')
-<div class="row">
-    <div class="col-lg-12 margin-tb">
-        <div class="pull-left">
-            <h2>Tambah data dosen</h2>
+    @if ($errors->any())
+        <div class="alert alert-danger">
+            <strong>Whoops!</strong> There were some problems with your input.<br><br>
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
         </div>
-        <div class="pull-right">
-            <a class="btn btn-primary" href="{{ route('lecturers.index') }}"> Back</a>
+    @endif
+
+
+    <div class="row">
+        <div class="col-sm-12">
+            <div class="card card-primary">
+                <div class="card-header">
+                    <h3 class="card-title">Masukkan Data Dosen </h3>
+                </div>
+                <form role="form" method="POST" action="{{ route('lecturers.store')  }}">
+                    @csrf
+                    <div class="card-body">
+                        <div class="row">
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="name">Nama Dosen</label>
+                                    <input type="text" class="form-control" id="name" name="name"
+                                           placeholder="Masukkan Nama Dosen" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <label for="nid">NID</label>
+                                    <input type="text" class="form-control" id="nid" name="nid"
+                                           placeholder="Masukkan NID" required>
+                                </div>
+                            </div>
+                            <div class="col-md-6 col-sm-12">
+                                <div class="form-group">
+                                    <label for="department">Department</label>
+                                    <input type="text" name="department" id="department" class="form-control"
+                                           placeholder="Masukkan Nama Department" required>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <div class="row">
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="sex" value="laki-laki"
+                                                   id="laki-laki">
+                                            <label class="form-check-label" for="laki-laki">Laki-Laki</label>
+                                        </div>
+
+                                        <div class="form-check">
+                                            <input class="form-check-input" type="radio" name="sex" value="perempuan"
+                                                   id="perempuan">
+                                            <label class="form-check-label" for="perempuan">Perempuan</label>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-sm-12">
+                                <div class="form-group">
+                                    <label for="address">Alamat</label>
+                                    <input type="text" name="address" id="address" class="form-control" required>
+                                </div>
+                            </div>
+                            <div class="col-sm-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="email">Email</label>
+                                    <input type="email" name="email" id="email" class="form-control">
+                                </div>
+                            </div>
+
+                            <div class="col-sm-12 col-md-6">
+                                <div class="form-group">
+                                    <label for="phone">Nomor Telepon</label>
+                                    <input type="text" name="phone" id="phone" class="form-control" required>
+                                </div>
+                            </div>
+                        </div>
+
+
+
+
+
+
+
+
+
+                        <div class="form-check">
+                            <input type="checkbox" class="form-check-input" id="is_tpkk_admin" name="is_tpkk_admin">
+                            <label class="form-check-label" for="is_tpkk_admin">Pengurus TPKK</label>
+                        </div>
+
+
+                    </div>
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                </form>
+            </div>
         </div>
     </div>
-</div>
-   
-@if ($errors->any())
-    <div class="alert alert-danger">
-        <strong>Whoops!</strong> There were some problems with your input.<br><br>
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
-            @endforeach
-        </ul>
-    </div>
-@endif
-   
-<form action="{{ route('lecturers.store') }}" method="POST">
-    @csrf
-  
-     <div class="row">
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Nama:</strong>
-                <input type="text" name="name" class="form-control" placeholder="Nama">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>NID:</strong>
-                <input type="text" name="nid" class="form-control" placeholder="NID">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Departemen:</strong>
-                <input type="text" name="department" class="form-control" placeholder="Departemen">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Jenis kelamin:</strong><br>
-                <input type="radio" id="male" name="sex" value="Laki-laki">
-                <label for="laki-laki">Laki-laki</label><br>
-                <input type="radio" id="female" name="sex" value="Perempuan">
-                <label for="perempuan">Perempuan</label><br>
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Alamat:</strong>
-                <input type="text" name="address" class="form-control" placeholder="Alamat">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Email:</strong>
-                <input type="email" name="email" class="form-control" placeholder="123@example.com">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12">
-            <div class="form-group">
-                <strong>Telepon:</strong>
-                <input type="text" name="phone" class="form-control" placeholder="0878xxxxxx">
-            </div>
-        </div>
-        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-                <button type="submit" class="btn btn-primary">Submit</button>
-        </div>
-    </div>
-   
-</form>
 @endsection
