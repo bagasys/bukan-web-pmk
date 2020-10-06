5e641377491a200891c7b5b9f1c85aed56e99776
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-06 23:51:59 +0700

    Update index.blade.php
    tambah pagination frontend

--- resources/views/meetings/index.blade.php
+++ resources/views/meetings/index.blade.php
@@ -9,14 +9,10 @@
 
     <div class="card">
         <div class="card-header">
-            <h3 class="card-title"><Meetings></Meetings></h3>
+            <h3 class="card-title">Tabel Meeting</h3>
 
-
-            <div class="card-tools">
-                <div class="">
-                    <a class="btn btn-success" href="{{ route('meetings.create') }}"> Buat Meeting Baru</a>
-                </div>
-            </div>
+            <a class="btn btn-info float-right bg-success" href="{{ route('meetings.create') }}"><i
+                    class="fa fa-plus"></i> Meeting Baru</a>
         </div>
 
         <!-- /.card-header -->
@@ -43,16 +39,27 @@
                             <form action="{{ route('meetings.destroy', $meeting->id) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
-                                <a class="btn btn-info" href="{{ route('meetings.show',$meeting->id) }}"><i class="fa fa-eye"></i></a>
-                                <a class="btn btn-primary" href="{{ route('meetings.edit',$meeting->id) }}"><i class="fa fa-edit"></i></a>
+                                <a class="btn btn-info" href="{{ route('meetings.show',$meeting->id) }}"><i
+                                        class="fa fa-eye"></i></a>
+                                <a class="btn btn-primary" href="{{ route('meetings.edit',$meeting->id) }}"><i
+                                        class="fa fa-edit"></i></a>
                                 <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i></button>
                             </form>
                         </td>
                     </tr>
                 @endforeach
                 </tbody>
+
             </table>
         </div>
+        <div class="card-footer">
+            <ul class="pagination pagination-md float-left">
+                <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
+                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
+            </ul>
+        </div>
     </div>
-
 @endsection
