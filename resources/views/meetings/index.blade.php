c777d431447391f27884d99a97bdda78c186c00e
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-06 23:02:26 +0700

    Update index.blade.php
    use icon as button

--- resources/views/meetings/index.blade.php
+++ resources/views/meetings/index.blade.php
@@ -43,9 +43,9 @@
                             <form action="{{ route('meetings.destroy', $meeting->id) }}" method="POST">
                                 @csrf
                                 @method('DELETE')
-                                <a class="btn btn-info" href="{{ route('meetings.show',$meeting->id) }}">Show</a>
-                                <a class="btn btn-primary" href="{{ route('meetings.edit',$meeting->id) }}">Edit</a>
-                                <button type="submit" class="btn btn-danger">Delete</button>
+                                <a class="btn btn-info" href="{{ route('meetings.show',$meeting->id) }}"><i class="fa fa-eye"></i></a>
+                                <a class="btn btn-primary" href="{{ route('meetings.edit',$meeting->id) }}"><i class="fa fa-edit"></i></a>
+                                <button type="submit" class="btn btn-danger"><i class="fa fa-trash"></i></button>
                             </form>
                         </td>
                     </tr>
