dfa19faa668a368082caa711e29efee111d48fa2
adriansantoso21 <mkadriansantoso@gmail.com>
2020-10-21 12:14:41 +0700

    sidebar finish

--- resources/views/adminlte/partials/sidebar.blade.php
+++ resources/views/adminlte/partials/sidebar.blade.php
@@ -1,6 +1,6 @@
 <aside class="main-sidebar sidebar-dark-primary elevation-4">
   <!-- Brand Logo -->
-  <a href="../../index3.html" class="brand-link">
+  <a href="#" class="brand-link">
     <img src="{{ asset('/adminlte/dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
     <span class="brand-text font-weight-light">AdminLTE 3</span>
   </a>
@@ -9,7 +9,7 @@
     <div class="sidebar">
       <!-- Sidebar Menu -->
       <nav class="mt-3">
-        <ul class="nav nav-pills nav-sidebar flex-column nav-tabs" data-widget="treeview" role="menu tablist" data-accordion="false">
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                 with font-awesome or any other icon font library -->
           <h2 class="text-center font-weight-bold mb-4"><span style="color:red;"><i class="nav-icon fas fa-cross "></i>PMK</span> <span style="color:#3366ff;">ITS</span></h2>
