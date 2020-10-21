e52122d724a0ce77079f5fc05e0fe47879b38531
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-21 21:57:15 +0700

    Update sidebar.blade.php

--- resources/views/adminlte/partials/sidebar.blade.php
+++ resources/views/adminlte/partials/sidebar.blade.php
@@ -23,9 +23,15 @@
             </a>
             <ul class="nav nav-treeview">
                 <li class="nav-item">
-                    <a href="{{route ('lecturers.index')}}" class="nav-link">
+                    <a href="{{route ('users.index')}}" class="nav-link">
                         <i class="far fa-circle nav-icon"></i>
-                        <p>User Management</p>
+                        <p>Users</p>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a href="{{route ('roles.index')}}" class="nav-link">
+                        <i class="far fa-circle nav-icon"></i>
+                        <p>Roles</p>
                     </a>
                 </li>
             </ul>
