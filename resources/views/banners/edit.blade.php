dccba62967719f13dac51c5e845102b62adc6098
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-22 00:32:01 +0700

    update dan create banner

--- resources/views/banners/edit.blade.php
+++ resources/views/banners/edit.blade.php
@@ -12,10 +12,15 @@
         </div>
     @endif
 
+
     <div class="row">
         <div class="col-sm-12">
-            <div class="card card-primary">
-                <form role="form" method="POST" action="{{ route('banners.update', $banner->id)  }}" enctype="multipart/form-data">
+            <div class="card ">
+                <div class="card-header">
+                    <h3 class="card-title">Edit Data Transaksi </h3>
+                </div>
+                <form role="form" method="POST" action="{{ route('transactions.update',$transaction->id) }}"
+                      enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
                     <div class="card-body">
@@ -24,7 +29,7 @@
                                 <div class="form-group">
                                     <label for="title">Title</label>
                                     <input type="text" class="form-control {{$errors->has('sender_name') ? 'is-invalid' : ''}}" id="title" name="title"
-                                           placeholder="Masukkan judul" value="{{old('title')}}">
+                                           placeholder="Masukkan judul" value="{{$banner->title}}">
                                     @error('title')
                                     <span class="error invalid-feedback">{{$message}}</span>
                                     @enderror
@@ -34,7 +39,7 @@
                                 <div class="form-group">
                                     <label for="subtitle">Subtitle</label>
                                     <input type="text" class="form-control {{$errors->has('subtitle') ? 'is-invalid' : ''}}" id="subtitle" name="subtitle"
-                                           placeholder="Masukkan subtitle" value="{{old('subtitle')}}">
+                                           placeholder="Masukkan subtitle" value="{{$banner->subtitle}}">
                                     @error('subtitle')
                                     <span class="error invalid-feedback">{{$message}}</span>
                                     @enderror
@@ -44,7 +49,7 @@
                                 <div class="form-group">
                                     <label for="description">Description</label>
                                     <textarea  rows="3" class="form-control {{$errors->has('description') ? 'is-invalid' : ''}}" id="description" name="description"
-                                    placeholder="Masukkan description">{{old('description')}}</textarea>
+                                               placeholder="Masukkan description">{{$banner->description}}</textarea>
                                     @error('description')
                                     <span class="error invalid-feedback">{{$message}}</span>
                                     @enderror
@@ -52,24 +57,23 @@
                             </div>
                             <div class="col-md-6 col-sm-12">
                                 <div class="form-group">
-                                    <label for="image">Gambar</label>
-                                    <input type="file" class="form-control {{$errors->has('proof') ? 'is-invalid' : ''}}" id="image" name="image"
+                                    <label for="image">Image</label>
+                                    <input type="file"
+                                           class="form-control {{$errors->has('image') ? 'is-invalid' : ''}}" id="image"
+                                           name="image"
                                     >
-                                    @error('proof')
+                                    @error('image')
                                     <span class="error invalid-feedback">{{$message}}</span>
                                     @enderror
                                 </div>
                             </div>
-
                         </div>
-                    </div>
 
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
+                        <div class="card-footer">
+                            <button type="submit" class="btn btn-primary">Submit</button>
+                        </div>
                 </form>
             </div>
         </div>
     </div>
 @endsection
-
