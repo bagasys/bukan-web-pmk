39e3d807915a550b46681930ae4722555002ba36
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-21 23:18:54 +0700

    pagination

--- resources/views/roles/index.blade.php
+++ resources/views/roles/index.blade.php
@@ -94,13 +94,7 @@
         </table>
     </div>
     <div class="card-footer">
-        <ul class="pagination pagination-md float-left">
-            <li class="page-item"><a class="page-link" href="#">&laquo;</a></li>
-            <li class="page-item"><a class="page-link" href="#">1</a></li>
-            <li class="page-item"><a class="page-link" href="#">2</a></li>
-            <li class="page-item"><a class="page-link" href="#">3</a></li>
-            <li class="page-item"><a class="page-link" href="#">&raquo;</a></li>
-        </ul>
+        {{$roles->links("pagination::bootstrap-4")}}
     </div>
 </div>
 
