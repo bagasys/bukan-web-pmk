a25349d35b107b6cf7e09db74151f1916709b00e
Bagas Yanuar Sudrajad <bagasys3@gmail.com>
2020-10-22 15:52:27 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -21,13 +21,13 @@
 <body class="hold-transition login-page">
     <div class="login-box">
       <div class="login-logo">
-        <a href="../../index2.html"><b>Admin</b>LTE</a>
+        <a href="../../index2.html"><b>Admin</b>PMK ITS</a>
       </div>
       <!-- /.login-logo -->
       <div class="card">
         <div class="card-body login-card-body">
           <p class="login-box-msg">Sign in to start your session</p>
-    
+
           <form action="{{route('login')}}" method="post">
             @csrf
             <div class="input-group mb-3">
@@ -57,14 +57,14 @@
               </span>
             @enderror
             <div class="row">
-              <div class="col-8">
-                <div class="icheck-primary">
-                  <input type="checkbox" id="remember">
-                  <label for="remember">
-                    Remember Me
-                  </label>
-                </div>
-              </div>
+{{--              <div class="col-8">--}}
+{{--                <div class="icheck-primary">--}}
+{{--                  <input type="checkbox" id="remember">--}}
+{{--                  <label for="remember">--}}
+{{--                    Remember Me--}}
+{{--                  </label>--}}
+{{--                </div>--}}
+{{--              </div>--}}
               <!-- /.col -->
               <div class="col-4">
                 <button type="submit" class="btn btn-primary btn-block">Sign In</button>
@@ -72,7 +72,7 @@
               <!-- /.col -->
             </div>
           </form>
-    
+
           {{-- <div class="social-auth-links text-center mb-3">
             <p>- OR -</p>
             <a href="#" class="btn btn-block btn-primary">
@@ -83,26 +83,26 @@
             </a>
           </div> --}}
           <!-- /.social-auth-links -->
-    
-          <p class="mb-1">
-            <a href="forgot-password.html">I forgot my password</a>
-          </p>
-          <p class="mb-0">
-            <a href="register.html" class="text-center">Register a new membership</a>
-          </p>
+
+{{--          <p class="mb-1">--}}
+{{--            <a href="forgot-password.html">I forgot my password</a>--}}
+{{--          </p>--}}
+{{--          <p class="mb-0">--}}
+{{--            <a href="register.html" class="text-center">Register a new membership</a>--}}
+{{--          </p>--}}
         </div>
         <!-- /.login-card-body -->
       </div>
     </div>
     <!-- /.login-box -->
-    
+
     <!-- jQuery -->
     <script src="../../plugins/jquery/jquery.min.js"></script>
     <!-- Bootstrap 4 -->
     <script src="../../plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
     <!-- AdminLTE App -->
     <script src="../../dist/js/adminlte.min.js"></script>
-    
+
     </body>
 <!-- Bootstrap 4 -->
 <script src="{{ asset('/adminlte/plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
