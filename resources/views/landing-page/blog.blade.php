6cd6b006b8202ebd1070c02a4c0d4057971a22bf
Jeremy Vijay Wongso <37033334+jeremyvw@users.noreply.github.com>
2020-10-31 23:19:28 +0700

    Connect frontend with backend

--- resources/views/landing-page/blog.blade.php
+++ resources/views/landing-page/blog.blade.php
@@ -16,102 +16,24 @@
 <section class="ftco-section">
     <div class="container">
         <div class="row d-flex">
-            <div class="col-md-6 col-lg-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('salvation/images/image_1.jpg');">
-                    </a>
-                    <div class="text p-4">
-                        <div class="meta mb-2">
-                            <div><a href="#">July 20, 2020</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
+            @foreach ($posts as $post)
+                <div class="col-md-6 col-lg-4 d-flex ftco-animate">
+                    <div class="blog-entry align-self-stretch">
+                        <a href="blog-single.html" class="block-20" style="background-image: url('{{$post->image}}');">
+                        </a>
+                        <div class="text p-4">
+                            <div class="meta mb-2">
+                                <div><a href="#">{{$post->created_at}}</a></div>
+                                <div><a href="#">{{$post->author}}</a></div>
+                                <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
+                            </div>
+                            <h3 class="heading"><a href="#">{{$post->title}}</a></h3>
+                            <p>{{$post->content}}</p>
+                            <p><a href="#" class="btn btn-primary">Read more</a></p>
                         </div>
-                        <h3 class="heading"><a href="#">Building Holy &amp; Healthy Lives God’s</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                        <p><a href="#" class="btn btn-primary">Read more</a></p>
                     </div>
                 </div>
-            </div>
-            <div class="col-md-6 col-lg-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('salvation/images/image_2.jpg');">
-                    </a>
-                    <div class="text p-4">
-                        <div class="meta mb-2">
-                            <div><a href="#">July 20, 2020</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading"><a href="#">Building Holy &amp; Healthy Lives God’s</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                        <p><a href="#" class="btn btn-primary">Read more</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('salvation/images/image_3.jpg');">
-                    </a>
-                    <div class="text p-4">
-                        <div class="meta mb-2">
-                            <div><a href="#">July 20, 2020</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading"><a href="#">Building Holy &amp; Healthy Lives God’s</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                        <p><a href="#" class="btn btn-primary">Read more</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('salvation/images/image_4.jpg');">
-                    </a>
-                    <div class="text p-4">
-                        <div class="meta mb-2">
-                            <div><a href="#">July 20, 2020</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading"><a href="#">Building Holy &amp; Healthy Lives God’s</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                        <p><a href="#" class="btn btn-primary">Read more</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('salvation/images/image_5.jpg');">
-                    </a>
-                    <div class="text p-4">
-                        <div class="meta mb-2">
-                            <div><a href="#">July 20, 2020</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading"><a href="#">Building Holy &amp; Healthy Lives God’s</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                        <p><a href="#" class="btn btn-primary">Read more</a></p>
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-6 col-lg-4 d-flex ftco-animate">
-                <div class="blog-entry align-self-stretch">
-                    <a href="blog-single.html" class="block-20" style="background-image: url('salvation/images/image_6.jpg');">
-                    </a>
-                    <div class="text p-4">
-                        <div class="meta mb-2">
-                            <div><a href="#">July 20, 2020</a></div>
-                            <div><a href="#">Admin</a></div>
-                            <div><a href="#" class="meta-chat"><span class="fa fa-comment"></span> 3</a></div>
-                        </div>
-                        <h3 class="heading"><a href="#">Building Holy &amp; Healthy Lives God’s</a></h3>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia</p>
-                        <p><a href="#" class="btn btn-primary">Read more</a></p>
-                    </div>
-                </div>
-            </div>
+            @endforeach
         </div>
         <div class="row mt-5">
             <div class="col text-center">
--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -14,8 +14,11 @@
 
     <div class="row">
         <div class="col-sm-12">
-            <div class="card card-primary">
-                <form role="form" method="POST" action="{{ route('posts.store')  }}" enctype="multipart/form-data">
+            <div class="card ">
+                <div class="card-header">
+                    <h3 class="card-title">Create New Post</h3>
+                </div>
+                    <form role="form" method="POST" action="{{ route('posts.store')}}" enctype="multipart/form-data">
                     @csrf
                     <div class="card-body">
                         <div class="row">
@@ -23,9 +26,9 @@
                                 <div class="form-group">
                                     <label for="title">Title</label>
                                     <input type="text" class="form-control {{$errors->has('title') ? 'is-invalid' : ''}}" id="title" name="title"
-                                           placeholder="Masukkan judul" value="{{old('title')}}">
+                                    placeholder="Masukkan judul" value="{{old('title')}}">
                                     @error('title')
-                                        <span class="error invalid-feedback">{{$message}}</span>
+                                    <span class="error invalid-feedback">{{$message}}</span>
                                     @enderror
                                 </div>
                             </div>
@@ -56,11 +59,11 @@
                                 <div class="form-group">
                                     <label for="author">Author</label>
                                     <input type="text" class="form-control {{$errors->has('author') ? 'is-invalid' : ''}}" id="author" name="author"
-                                           placeholder="Masukkan nama penulis" value="{{old('author')}}">
+                                    placeholder="Masukkan nama penulis" value="{{old('author')}}">
                                     @error('author')
                                         <span class="error invalid-feedback">{{$message}}</span>
-                                    @enderror
-                                </div>
+                                        @enderror
+                                    </div>
                             </div>
                             <div class="col-md-6 col-sm-12">
                                 <div class="form-group">
@@ -68,14 +71,14 @@
                                     <input type="file" class="form-control {{$errors->has('image') ? 'is-invalid' : ''}}" id="image" name="image"
                                     >
                                     @error('image')
-                                        <span class="error invalid-feedback">{{$message}}</span>
+                                    <span class="error invalid-feedback">{{$message}}</span>
                                     @enderror
                                 </div>
                             </div>
-
+                            
                         </div>
                     </div>
-
+                    
                     <div class="card-footer">
                         <button type="submit" class="btn btn-primary">Submit</button>
                     </div>
@@ -83,4 +86,4 @@
             </div>
         </div>
     </div>
-@endsection
+    @endsection
