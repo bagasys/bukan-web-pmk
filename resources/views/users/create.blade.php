9f2bc41aa5801e1fc2f8c8cbecb8e07b5523d672
kevinanggaaa <kevin.anggahung@gmail.com>
2020-12-12 11:28:24 +0700

    fix

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -1,90 +1,90 @@
 @extends('adminlte.master')
 
 @section('content')
-    @if ($errors->any())
-        <div class="alert alert-danger">
-            <strong>Whoops!</strong> There were some problems with your input.<br><br>
-            <ul>
-                @foreach ($errors->all() as $error)
-                    <li>{{ $error }}</li>
-                @endforeach
-            </ul>
-        </div>
-    @endif
+@if ($errors->any())
+<div class="alert alert-danger">
+    <strong>Whoops!</strong> There were some problems with your input.<br><br>
+    <ul>
+        @foreach ($errors->all() as $error)
+        <li>{{ $error }}</li>
+        @endforeach
+    </ul>
+</div>
+@endif
 
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1>User</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">User</li>
-                    </ol>
-                </div>
+<!-- Content Header (Page header) -->
+<section class="content-header">
+    <div class="container-fluid">
+        <div class="row mb-2">
+            <div class="col-sm-6">
+                <h1>User</h1>
             </div>
-        </div><!-- /.container-fluid -->
-    </section>
+            <div class="col-sm-6">
+                <ol class="breadcrumb float-sm-right">
+                    <li class="breadcrumb-item"><a href="#">Home</a></li>
+                    <li class="breadcrumb-item active">User</li>
+                </ol>
+            </div>
+        </div>
+    </div><!-- /.container-fluid -->
+</section>
 
 
-    <div class="row">
-        <div class="col-sm-12">
-            <div class="card card-primary">
-                <div class="card-header">
-                    <h3 class="card-title">Create new user</h3>
-                </div>
-                <form role="form" method="POST" action="{{ route('users.store')  }}">
-                    @csrf
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label for="email">Email</label>
-                                    <input type="email" class="form-control" id="email" name="email" placeholder="Masukkan email" required>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label for="email">Password</label>
-                                    <input type="password" class="form-control" id="password" name="password" placeholder="Masukkan password" required>
-                                </div>
-                            </div>
-                            <div class="col-12">
-                                <div class="form-group">
-                                    <label>Roles:</label>
-                                    <select class="duallistbox" multiple="multiple" name="role_id[]">
-                                        @foreach($roles as $role)
-                                            <option value="{{$role->id}}">{{$role->name}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
-                                <!-- /.form-group -->
+<div class="row">
+    <div class="col-sm-12">
+        <div class="card card-primary">
+            <div class="card-header">
+                <h3 class="card-title">Create new user</h3>
+            </div>
+            <form role="form" method="POST" action="{{ route('users.store')  }}">
+                @csrf
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="email">Email</label>
+                                <input class="form-control" id="email" name="email" placeholder="Masukkan email" required>
                             </div>
                         </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label for="email">Password</label>
+                                <input type="password" class="form-control" id="password" name="password" placeholder="Masukkan password" required>
+                            </div>
+                        </div>
+                        <div class="col-12">
+                            <div class="form-group">
+                                <label>Roles:</label>
+                                <select class="duallistbox" multiple="multiple" name="role_id[]">
+                                    @foreach($roles as $role)
+                                    <option value="{{$role->id}}">{{$role->name}}</option>
+                                    @endforeach
+                                </select>
+                            </div>
+                            <!-- /.form-group -->
+                        </div>
                     </div>
+                </div>
 
-                    <div class="card-footer">
-                        <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
-                </form>
-            </div>
+                <div class="card-footer">
+                    <button type="submit" class="btn btn-primary">Submit</button>
+                </div>
+            </form>
         </div>
     </div>
+</div>
 @endsection
 
 @push('scripts')
 
-    <script src="{{ asset('/adminlte/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.js')}}"></script>
-    <script>
-        $duallistbox = $('.duallistbox')
-        $duallistbox.bootstrapDualListbox({
-            nonSelectedListLabel: 'Available Roles',
-            selectedListLabel: 'Chosen Roles',
-            moveOnSelect: false,
-            moveAllLabel: 'aaa'
-        });
-    </script>
+<script src="{{ asset('/adminlte/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.js')}}"></script>
+<script>
+    $duallistbox = $('.duallistbox')
+    $duallistbox.bootstrapDualListbox({
+        nonSelectedListLabel: 'Available Roles',
+        selectedListLabel: 'Chosen Roles',
+        moveOnSelect: false,
+        moveAllLabel: 'aaa'
+    });
+</script>
 @endpush
